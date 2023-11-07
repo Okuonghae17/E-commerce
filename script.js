059402224f8f192e1440422ff48905fd37307274
David <dokuonghae256@gmail.com>
2023-11-07 23:16:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simulate adding products to the cart
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Product added to cart. Continue shopping or proceed to checkout.');
+    });
+});
